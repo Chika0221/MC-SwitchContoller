f799e370fea9de18bc0d42147bed40ab50329926
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2026-02-27 16:22:20 +0900

    connect_providerをインポートし、useEffectを追加してアプリ起動時に接続を切断する処理を実装

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,13 @@ import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rive/rive.dart';
 
 // Project imports:
 import 'package:switch_controller/firebase_options.dart';
+import 'package:switch_controller/infrastructure/providers/connect_provider.dart';
 import 'package:switch_controller/infrastructure/providers/route_provider.dart';
 import 'package:switch_controller/theme/custom_theme.dart';
 
@@ -26,6 +28,12 @@ class SwitchControllerApp extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final cTheme = CustomTheme();
+
+    useEffect(() {
+      return () {
+        ref.read(connectProvider.notifier).disconnect();
+      };
+    }, []);
 
     return MaterialApp.router(
       themeMode: ThemeMode.system,
